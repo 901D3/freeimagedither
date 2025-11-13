319af56094e15b4b3d821888c4b1fba4d6c8ca6c
🗣🗣: "ùồầỳ hợp lý quãi chất thế nhờ"🤫🗿🔥🔥 <148612010+901D3@users.noreply.github.com>
2025-11-13 15:41:14 +0700

    Update process.js
    Include dotDiffs

--- scripts/process.js
+++ scripts/process.js
@@ -4,24 +4,26 @@ const d = {
   arithmetic: arithmetic,
   errDiffs: errDiffs,
   varErrDiffs: varErrDiffs,
+  dotDiffs: dotDiffs,
 };
 
 function process() {
   const t0 = performance.now();
   processFrame();
-  if (ditherDropdownValue !== "none") printLog("Dithering took " + (performance.now() - t0) + "ms");
+  if (ditherDropdownValue !== "none")
+    printLog("Dithering took " + (performance.now() - t0) + "ms");
 }
 
 function processFrame() {
   ctx.drawImage(image, 0, 0, canvasWidth, canvasHeight);
   const frame = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
   const imageData = frame.data;
-  const imageDataLength = imageData.length;
   if (useLinear) {
-    for (let i = 0; i < imageDataLength; i++) {
+    for (let i = 0, length = imageData.length; i < length; i++) {
       imageData[i] = floor(linearLUT[imageData[i]]);
     }
   }
+
   d[ditherDropdownValue](imageData);
 
   ctx.putImageData(frame, 0, 0);
