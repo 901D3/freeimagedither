ce5697228705739bfa71b7d8eb37e94c8e50c3cc
🗣🗣: "ùồầỳ hợp lý quãi chất thế nhờ"🤫🗿🔥🔥 <148612010+901D3@users.noreply.github.com>
2025-12-01 16:38:55 +0700

    Update generator.js

--- scripts/generator.js
+++ scripts/generator.js
@@ -2,7 +2,6 @@
 
 (function () {
   const scriptElement = document.createElement("script");
-  scriptElement.type;
   scriptElement.src = "https://901d3.github.io/freevideodither/scripts/generator.js";
 
   document.head.appendChild(scriptElement);
