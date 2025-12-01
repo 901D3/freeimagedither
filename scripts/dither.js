e497cc29f3610f9556b8d146509d9b4d094f1d77
🗣🗣: "ùồầỳ hợp lý quãi chất thế nhờ"🤫🗿🔥🔥 <148612010+901D3@users.noreply.github.com>
2025-12-01 18:17:13 +0700

    Update dither.js

--- scripts/dither.js
+++ scripts/dither.js
@@ -5,4 +5,13 @@
   scriptElement.src = "https://901d3.github.io/freevideodither/scripts/dither.js";
 
   document.head.appendChild(scriptElement);
+
+  scriptElement.addEventListener("load", () => {
+    d["none"] = () => {};
+    d["matrixThreshold"] = bayer;
+    d["arithmetic"] = arithmetic;
+    d["errDiffs"] = errDiffs;
+    d["varErrDiffs"] = varErrDiffs;
+    d["dotDiffs"] = dotDiffs;
+  });
 })();
