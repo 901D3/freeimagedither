a2e1317d50cb06f13bee4d29a50c7eba0ae72917
🗣🗣: "ùồầỳ hợp lý quãi chất thế nhờ"🤫🗿🔥🔥 <148612010+901D3@users.noreply.github.com>
2025-12-01 16:37:07 +0700

    Update varErrDiffsRsrc.zhouFang.js

--- scripts/resources/varErrDiffsRsrc.zhouFang.js
+++ scripts/resources/varErrDiffsRsrc.zhouFang.js
@@ -2,8 +2,8 @@
 
 (function () {
   const scriptElement = document.createElement("script");
-  scriptElement.type;
   scriptElement.src = "https://901d3.github.io/freevideodither/scripts/resources/varErrDiffsRsrc.zhouFang.js";
 
   document.head.appendChild(scriptElement);
 })();
+
