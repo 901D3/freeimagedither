aedda7b5d77d0cc9d44cbf5142d23dd9da61de7f
🗣🗣: "ùồầỳ hợp lý quãi chất thế nhờ"🤫🗿🔥🔥 <148612010+901D3@users.noreply.github.com>
2025-12-01 16:38:44 +0700

    Update dither.js

--- scripts/dither.js
+++ scripts/dither.js
@@ -2,7 +2,6 @@
 
 (function () {
   const scriptElement = document.createElement("script");
-  scriptElement.type;
   scriptElement.src = "https://901d3.github.io/freevideodither/scripts/dither.js";
 
   document.head.appendChild(scriptElement);
